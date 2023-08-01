687bbf65c04bcca777642268e69bb06d45eed63b
ashiqy04 <ashiqy04@gmail.com>
2023-08-01 12:57:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import {orginals, action} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <NavBar />
+        <Banner />
+        <RowPost url={orginals} title='Netflix Orginals' />
+        <RowPost url={action} title='Action' isSmall />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '7a8e4bfbec912f43e88dd20103bd8fc7';
+export const imageUrl=  'https://image.tmdb.org/t/p/original';
